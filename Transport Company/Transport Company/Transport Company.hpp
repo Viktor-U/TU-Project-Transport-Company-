59614c2b0cfb1be0d89655541da711e7bfdedc20
Viktor Uzunov <125596869+Viktor-U@users.noreply.github.com>
2024-12-10 14:49:33 +0200

    Alex Work

--- Transport Company/Transport Company/Transport Company.hpp
+++ Transport Company/Transport Company/Transport Company.hpp
@@ -9,5 +9,31 @@
 #define Transport_Company_hpp
 
 #include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class TransportCompany{
+protected:
+    char name[20];
+    char addres[20];
+    //do array wiht Vehicle
+    int vehicleAmount;
+    //do array wiht Drivers
+    int driversAmount;
+
+    
+    
+public:
+    TransportCompany();
+    TransportCompany(const char nameI[20], const char addresI[20] );
+    //void addVehicle() to do
+    //void addDriver() to do
+    //void defineTrack() to do
+    //void printFreeVehicles() to do
+    
+};
+
 
 #endif /* Transport_Company_hpp */
