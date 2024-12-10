1db702d956b66edf586c80b6c5450075016d1680
Viktor Uzunov <125596869+Viktor-U@users.noreply.github.com>
2024-12-10 12:53:40 +0200

    first commit

---
+++ Transport Company/Transport Company/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Transport Company
+//
+//  Created by Viktor Uzunov on 10.12.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
